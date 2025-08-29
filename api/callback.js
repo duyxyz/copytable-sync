4b6c7ba497e2f92f34369cf097b2d3afa802af61
Nguyễn Minh Duy <ngduy10102006@gmail.com>
2025-08-29 22:46:10 +0700

    Add files via upload

---
+++ api/callback.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+  const code = req.query.code;
+
+  if (!code) {
+    return res.status(400).json({ error: "Missing code" });
+  }
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code
+    })
+  });
+
+  const data = await response.json();
+
+  if (data.error) {
+    return res.status(400).json(data);
+  }
+
+  // Trả token cho frontend
+  return res.redirect(`/?token=${data.access_token}`);
+}
